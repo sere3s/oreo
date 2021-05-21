a9c9deae7d4a00fe8e02a6e4e042d60d67a5cf6b
Giorgio Luciano <giorgio.luciano@gmail.com>
2021-05-21 13:38:24 +0200

    Update Rpp_plot_v3.r

--- R/Rpp_plot_v3.r
+++ R/Rpp_plot_v3.r
@@ -24,8 +24,8 @@
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25 
 
-plotStressStrain <- function(stress, strain,strain_in,stress_in,...){
-  plot(strain, stress , main="stress-strain", xlab="Strain [-]", ylab ="Stress [Pa]",type="l", col="red",lwd=2)
+plotStressStrain <- function(stress, strain,strain_in,stress_in,type="l",...){
+  plot(strain, stress , main="stress-strain", xlab="Strain [-]", ylab ="Stress [Pa]", col="red",lwd=2,...)
   lines(strain_in,stress_in, col="blue")
   legend("topleft",legend=c("raw stress", "reconstructed"),col=c("red", "blue"), lty=1:2, cex=0.6)
   abline(h=0, v=0,lty=2)
@@ -48,14 +48,18 @@ plotStressStrain <- function(stress, strain,strain_in,stress_in,...){
 #' out <- Rpp_num(time_wave, resp_wave , L=1024, k=8, num_mode=1)
 #' rate= out$spp_data_out$rate
 #' stress= out$spp_data_out$stress
+#' rate_in= out$spp_data_in$strain_rate
+#' stress_in= out$spp_data_in$stress
 #' plotStressRate(stress, rate)}
 #' 
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25 
 
-plotStressRate <- function(stress, rate,...){
-  plot(stress,rate,main="stress-rate",xlab ="Rate [1/s]", ylab="Stress [Pa]",type="l", col="red",lwd=2)
+plotStressRate <- function(stress, rate,type="l",...){
+  plot(stress,rate,main="stress-rate",xlab ="Rate [1/s]", ylab="Stress [Pa]",, col="red",lwd=2,...)
+  lines(strain_in,stress_in, col="blue")
+  legend("topleft",legend=c("raw stress", "reconstructed"),col=c("red", "blue"), lty=1:2, cex=0.6)
   abline(h=0, v=0,lty=2)
 }
 
@@ -82,8 +86,8 @@ plotStressRate <- function(stress, rate,...){
 #' @import graphics
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25 
-plotColeCole <- function(Gp_t,Gpp_t,...){
-  plot(Gp_t,Gpp_t,main="Cole-Cole plot",xlab=expression("G'"[t]*" [Pa]"),ylab=expression("G''"[t]*" [Pa]"),type="l", col="red",lwd=2)
+plotColeCole <- function(Gp_t,Gpp_t,type="l",...){
+  plot(Gp_t,Gpp_t,main="Cole-Cole plot",xlab=expression("G'"[t]*" [Pa]"),ylab=expression("G''"[t]*" [Pa]"), col="red",lwd=2,...)
   abline(h=0,v=0, lty=2)
   abline(a=c(0,0), b=c(1,1),lty=2)
 }
@@ -112,8 +116,8 @@ plotColeCole <- function(Gp_t,Gpp_t,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25 
-plotVGP <- function(G_star_t,delta_t,...){
-  plot(G_star_t,delta_t,main="VGP plot",xlab=expression("G*"[t]*" [Pa]"),ylab=expression("delta"[t]*" [rad]"),type="l", col="red",lwd=2)
+plotVGP <- function(G_star_t,delta_t,type="l",...){
+  plot(G_star_t,delta_t,main="VGP plot",xlab=expression("G*"[t]*" [Pa]"),ylab=expression("delta"[t]*" [rad]"), col="red",lwd=2,...)
   abline(h=0, v=0,lty=2)}
 
 
@@ -140,8 +144,8 @@ plotVGP <- function(G_star_t,delta_t,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25 
-plotGpdot <- function(Gp_t_dot,Gpp_t_dot,...){
-  plot(Gp_t_dot,Gpp_t_dot,main=expression("dG'"[t]*"/dt-dG''"[t]*"/dt"), xlab=expression("dG'"[t]*"/dt [Pa/s]"),ylab=expression("dG''"[t]*"/dt [Pa/s]"),type="l", col="red",lwd=2 ) 
+plotGpdot <- function(Gp_t_dot,Gpp_t_dot,type="l",...){
+  plot(Gp_t_dot,Gpp_t_dot,main=expression("dG'"[t]*"/dt-dG''"[t]*"/dt"), xlab=expression("dG'"[t]*"/dt [Pa/s]"),ylab=expression("dG''"[t]*"/dt [Pa/s]"), col="red",lwd=2,... ) 
 }
 
 
@@ -166,8 +170,8 @@ plotGpdot <- function(Gp_t_dot,Gpp_t_dot,...){
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
 
-plotSpeedGp <- function(Gp_t,G_speed,...){
-  plot(Gp_t,G_speed,main=expression("Speed-G'"[t]*"") ,xlab=expression("G'"[t]*" [Pa]"),ylab=expression("Speed-G''"[t]*" [Pa/s]"),type="l", col="red",lwd=2 )}
+plotSpeedGp <- function(Gp_t,G_speed,type="l",...){
+  plot(Gp_t,G_speed,main=expression("Speed-G'"[t]*"") ,xlab=expression("G'"[t]*" [Pa]"),ylab=expression("Speed-G''"[t]*" [Pa/s]"), col="red",lwd=2,... )}
 
 
 
@@ -192,8 +196,8 @@ plotSpeedGp <- function(Gp_t,G_speed,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotSpeedGpp <- function(G_speed,Gpp_t,...){
-  plot(G_speed,Gpp_t,main=expression("Speed-G''"[t]*"") ,xlab=expression("Speed [Pa/s]"),ylab=expression("G''"[t]*" [Pa]"),type="l", col="red",lwd=2 )
+plotSpeedGpp <- function(G_speed,Gpp_t,type="l",...){
+  plot(G_speed,Gpp_t,main=expression("Speed-G''"[t]*"") ,xlab=expression("Speed [Pa/s]"),ylab=expression("G''"[t]*" [Pa]"), col="red",lwd=2 )
 }
 
 
@@ -218,8 +222,8 @@ plotSpeedGpp <- function(G_speed,Gpp_t,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotDeltaStrain <- function(strain,delta_t,...){
-  plot(strain,delta_t,main=expression("delta"[t]*"-strain"),xlab=expression("strain [-]"),ylab=expression("delta"[t]*" [rad]"),type="l", col="red",lwd=2 )}
+plotDeltaStrain <- function(strain,delta_t,type="l",...){
+  plot(strain,delta_t,main=expression("delta"[t]*"-strain"),xlab=expression("strain [-]"),ylab=expression("delta"[t]*" [rad]"), col="red",lwd=2,... )}
 
 
 
@@ -241,8 +245,8 @@ plotDeltaStrain <- function(strain,delta_t,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotPAV  <- function(strain,delta_t_dot,...){
-  plot(strain,delta_t_dot,main="PAV-strain",xlab=expression("strain [-]"),ylab=expression("PAV [] (time-normalized)"),type="l", col="red", lwd=2)}
+plotPAV  <- function(strain,delta_t_dot,type="l",...){
+  plot(strain,delta_t_dot,main="PAV-strain",xlab=expression("strain [-]"),ylab=expression("PAV [] (time-normalized)"), col="red", lwd=2,...)}
 
 
 
@@ -265,8 +269,8 @@ plotPAV  <- function(strain,delta_t_dot,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotDisp  <- function(strain,disp_stress,...){
-  plot(strain,disp_stress,main="displacement stress-strain",xlab=expression("strain [-]"),ylab=expression("displacement stress [Pa]"),type="l", col="red", lwd=2 )}
+plotDisp  <- function(strain,disp_stress,type="l",...){
+  plot(strain,disp_stress,main="displacement stress-strain",xlab=expression("strain [-]"),ylab=expression("displacement stress [Pa]"), col="red", lwd=2,... )}
 
 
 
@@ -289,8 +293,8 @@ plotDisp  <- function(strain,disp_stress,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotStrain <- function(Gp_t,eq_strain_est,...){
-  plot(Gp_t,eq_strain_est,main="est. eq. strain-tan(delta)",xlab=expression("G'"[t]*" [Pa]"),ylab=expression("esp eq. strain[-]"),type="l", col="red", lwd=2 )}
+plotStrain <- function(Gp_t,eq_strain_est,type="l",...){
+  plot(Gp_t,eq_strain_est,main="est. eq. strain-tan(delta)",xlab=expression("G'"[t]*" [Pa]"),ylab=expression("esp eq. strain[-]"), col="red", lwd=2,... )}
 
 
 #Waveform Comparison
@@ -316,8 +320,8 @@ plotStrain <- function(Gp_t,eq_strain_est,...){
 #' @export
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
-plotTimeStrain <- function(time_wave,strain,time_wave_in,strain_in, ...){
-  plot(time_wave,strain,main="Strain-time",xlab=expression("Time [s]"),ylab = expression("Strain [-]"),type="l", col="red",xlim=c(0,3), lwd=2)
+plotTimeStrain <- function(time_wave,strain,time_wave_in,strain_in,type="l", ...){
+  plot(time_wave,strain,main="Strain-time",xlab=expression("Time [s]"),ylab = expression("Strain [-]"), col="red",xlim=c(0,3), lwd=2,...)
   lines(time_wave_in,strain_in,lty=2)}
 
 
@@ -345,8 +349,8 @@ plotTimeStrain <- function(time_wave,strain,time_wave_in,strain_in, ...){
 #' @references Simon A. Rogersa, M. Paul Letting, A sequence of physical processes determined and quantified in large-amplitude oscillatory shear (LAOS): Application to theoretical nonlinear models
 #' Journal of Rheology 56:1, 1-25
 
-plotTimeRate <- function(time_wave,rate,time_wave_in,strain_rate,...){
-  plot(time_wave,rate,main="Rate-time",xlab=expression("Time [s]"),ylab = expression("Rate [1/s]"),type="l", col="red",xlim=c(0,3), lwd=2)
+plotTimeRate <- function(time_wave,rate,time_wave_in,strain_rate,type="l",...){
+  plot(time_wave,rate,main="Rate-time",xlab=expression("Time [s]"),ylab = expression("Rate [1/s]"), col="red",xlim=c(0,3), lwd=2,...)
   lines(time_wave_in,strain_rate,lty=2)}
 
 #' Stress-Time plot
@@ -372,8 +376,8 @@ plotTimeRate <- function(time_wave,rate,time_wave_in,strain_rate,...){
 #' 
 #' @export
 
-plotTimeStress <- function(time_wave,stress,time_wave_in,strain_rate,...){
-  plot(time_wave,stress,main="Stress-time",xlab=expression("Time [s]"),ylab=expression("Stress [Pa]"),type="l", col="red", xlim=c(0,3), lwd=2)
+plotTimeStress <- function(time_wave,stress,time_wave_in,strain_rate,type="l",...){
+  plot(time_wave,stress,main="Stress-time",xlab=expression("Time [s]"),ylab=expression("Stress [Pa]"), col="red", xlim=c(0,3), lwd=2,...)
   lines(time_wave_in,strain_rate,lty=2)}
 
 #' Stress-Time plot
@@ -398,8 +402,8 @@ plotTimeStress <- function(time_wave,stress,time_wave_in,strain_rate,...){
 #' 
 #' @export
 
-plotStressTime <- function(time_wave_in,stress_in,time_wave,stress){
-  plot(time_wave_in,stress_in,xlab=expression("Time[s]"),ylab=expression("Stress [Pa]"),type="l", col="black",lty=2 )
+plotStressTime <- function(time_wave_in,stress_in,time_wave,stress,type="l",){
+  plot(time_wave_in,stress_in,xlab=expression("Time[s]"),ylab=expression("Stress [Pa]"), col="black",lty=2,... )
   lines(time_wave,stress,main="Stress-time",type="l", col="red", lwd=2 )}
 
 #' Fourier Harmonic Magnitudes plot
@@ -422,8 +426,8 @@ plotStressTime <- function(time_wave_in,stress_in,time_wave,stress){
 #' plotFft(ft_amp,fft_resp,spp_params)}
 #' 
 #' @export
-plotFft <- function(ft_amp,fft_resp,spp_params,...){
-  plot(ft_amp,fft_resp,main="Fourier spectrum",xlab=expression("Number of harmonics [-]"),ylab=expression(paste(I[n],"/",I[1],"[-]")),log="y")
+plotFft <- function(ft_amp,fft_resp,spp_params,type="l",...){
+  plot(ft_amp,fft_resp,main="Fourier spectrum",xlab=expression("Number of harmonics [-]"),ylab=expression(paste(I[n],"/",I[1],"[-]")),log="y",...)
   segments(x0=as.numeric(spp_params[2]), y0=10^-10, x1 = as.numeric(spp_params[2]), y1 = 0.7,col="red",lwd=2)}
 
 
